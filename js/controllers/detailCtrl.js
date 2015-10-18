bf27a82c739c4fad596922634b04cf4b120e4879
J R Bennett <justinbennett@outlook.com>
2015-10-18 16:07:09 -0400

    Deatils page functioning

--- js/controllers/detailCtrl.js
+++ js/controllers/detailCtrl.js
@@ -2,6 +2,24 @@ angular.module('celebsApp')
 
 .controller('DetailCtrl', function($scope, $http, Celebs, $routeParams) {
 
+        $scope.celebrityId = $routeParams.celebId;
+        $scope.celebrity = {};
 
+        Celebs.getOne( $scope.celebrityId )
+            .then(function( response ) {
+
+               var celebrities = response.data.celebs;
+                celebrities.forEach(function( i ) {
+                    if( i.id == $scope.celebrityId ) {
+                        $scope.celebrity = i;
+                    }
+                });
+                console.log($scope.celebrity);
+
+            }, function( response ){
+
+                //error
+
+            });
 
 });
--- js/routes.js
+++ js/routes.js
@@ -1,19 +1,19 @@
 angular.module('celebsApp')
 
 .config(function($routeProvider) {
-    
+
     $routeProvider
     .when('/', {
         templateUrl: 'templates/list.html',
         controller: 'ListCtrl'
     })
-    
+
     // Don't forget to use [:myVar] for dynamic data
-    .when('/detail', {
-        templateUrl: '/templates/detail.html',
+    .when('/detail/:celebId', {
+        templateUrl: 'templates/detail.html',
         controller: 'DetailCtrl'
     })
-    
+
     .otherwise({redirectTo: '/'});
-    
+
 })
